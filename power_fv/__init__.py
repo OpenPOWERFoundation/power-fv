b60aaae27b5226829905257dc1d5ab50e437e842
Jean-François Nguyen <jf@jfng.fr>
2022-04-01 17:41:26 +0200

    build.plat: add SymbiYosysPlatform.

--- power_fv/__init__.py
+++ power_fv/__init__.py
@@ -4,9 +4,11 @@ __version__ = metadata.version(__package__)
 
 from .dut import *
 from .tb import *
+from .build import *
 
 
 __all__ = [
     "Interface",
     "Testbench",
+    "SymbiYosysPlatform",
 ]

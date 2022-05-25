OpenPOWERFoundation/power-fv

commit 9ea58a47a9897ba11daae73c6c92141037f164e5

Refactor to facilitate integration with CLIs and config files.
* Checks are now split in two modules: checks.cons for consistency
  checks, checks.insn for instructions.

* Checks are derived from PowerFVCheck and have a shorthand (e.g.
  "insn_b"). PowerFVCheck holds a mapping between its subclasses and
  their shorthands.

* Instruction checks definitions have been simplified to one-liners,
  and grouped into a single file.

* A Trigger class has been added to define testbench triggers.

power_fv/tb.py
@@ -1,51 +1,78 @@
+from collections import OrderedDict
+
 from amaranth import *
 from amaranth.asserts import *
+from amaranth.utils import bits_for
 
 
-__all__ = ["Testbench"]
+__all__ = ["Trigger", "Testbench"]
 
 
-def _check_triggers(t_start, t_pre, t_post):
-    if not isinstance(t_start, int) or t_start <= 0:
-        raise ValueError("t_start must be a positive integer, not {!r}"
-                         .format(t_start))
-    if not isinstance(t_post, int) or t_post < t_start:
-        raise ValueError("t_post must be an integer greater than or equal to t_start ({}), not {!r}"
-                         .format(t_start, t_post))
-    if not isinstance(t_pre, int) or t_pre > t_post or t_pre < t_start:
-        raise ValueError("t_pre must be an integer between t_start and t_post (i.e. [{},{}]), "
-                         "not {!r}".format(t_start, t_post, t_pre))
+class Trigger:
+    def __init__(self, cycle, name=None, src_loc_at=0):
+        if not isinstance(cycle, int) or cycle < 0:
+            raise ValueError("Clock cycle must be a non-negative integer, not {!r}"
+                             .format(cycle))
+
+        self.stb   = Signal(name=name, src_loc_at=1 + src_loc_at)
+        self.cycle = cycle
 
 
 class Testbench(Elaboratable):
-    def __init__(self, check, dut, *, t_start=1, t_pre=None, t_post=20):
-        if t_pre is None and t_post is not None:
-            t_pre = t_post
+    def __init__(self, spec, dut, start=0):
+        self.spec  = spec
+        self.dut   = dut
+        self.start = Trigger(cycle=start)
 
-        _check_triggers(t_start, t_pre, t_post)
+        self._triggers  = OrderedDict()
+        self._bmc_depth = None
+        self._frozen    = False
 
-        self.check   = check
-        self.dut     = dut
-        self.t_start = t_start
-        self.t_pre   = t_pre
-        self.t_post  = t_post
+        self.add_trigger(self.start)
+        for trigger in spec.triggers():
+            self.add_trigger(trigger)
+
+    def freeze(self):
+        if not self._frozen:
+            self._bmc_depth = max(t.cycle for t in self.triggers()) + 1
+            self._frozen    = True
+
+    def add_trigger(self, trigger):
+        if self._frozen:
+            raise ValueError("Testbench is frozen.")
+        if not isinstance(trigger, Trigger):
+            raise TypeError("Trigger must be an instance of Trigger, not {!r}"
+                            .format(trig))
+        self._triggers[id(trigger)] = trigger
+
+    def triggers(self):
+        yield from self._triggers.values()
+
+    @property
+    def bmc_depth(self):
+        self.freeze()
+        return self._bmc_depth
 
     def elaborate(self, platform):
         m = Module()
 
-        timer = Signal(range(self.t_post + 2), reset=1)
+        cycle = Signal(range(self.bmc_depth), reset=0)
 
-        with m.If(timer <= self.t_post):
-            m.d.sync += timer.eq(timer + 1)
+        with m.If(cycle < self.bmc_depth):
+            m.d.sync += cycle.eq(cycle + 1)
 
-        m.submodules.check = ResetInserter(timer < self.t_start)(self.check)
-        m.submodules.dut   = self.dut
+        for trigger in self.triggers():
+            m.d.comb += trigger.stb.eq(cycle == trigger.cycle)
 
-        m.d.comb += [
-            self.check.pfv.eq(self.dut.pfv),
-            self.check.trig.pre .eq(timer == self.t_pre),
-            self.check.trig.post.eq(timer == self.t_post),
-        ]
+        spec_rst = Signal(reset=1)
+
+        with m.If(self.start.stb):
+            m.d.sync += spec_rst.eq(0)
+
+        m.submodules.spec = ResetInserter(spec_rst)(self.spec)
+        m.submodules.dut  = self.dut
+
+        m.d.comb += self.spec.pfv.eq(self.dut.pfv)
 
         m.d.comb += Assume(ResetSignal() == Initial())
 

cores/microwatt/run.py
@@ -1,11 +1,11 @@
 import argparse
-import importlib
 import os
 import multiprocessing
 
 from amaranth import *
 
-from power_fv.tb import Testbench
+from power_fv.checks import *
+from power_fv.checks.all import *
 from power_fv.build import SymbiYosysPlatform
 
 from _wrapper import MicrowattWrapper
@@ -58,22 +58,23 @@ def microwatt_files():
     yield top_filename, top_contents
 
 
-def run_check(module_name, pre, post):
-    module = importlib.import_module(f".{module_name}", package="power_fv.checks")
-    check  = module.Check()
-    cpu    = MicrowattWrapper()
-    top    = Testbench(check, cpu, t_pre=pre, t_post=post)
+def run_check(*args):
+    check_name, tb_args = args
+
+    check = PowerFVCheck.registry[check_name]()
+    dut   = MicrowattWrapper()
+    tb    = check.get_testbench(dut, **tb_args)
 
     platform = SymbiYosysPlatform()
     for filename, contents in microwatt_files():
         platform.add_file(filename, contents)
 
-    top_name  = "{}_tb".format(module_name)
-    build_dir = "build/{}".format(top_name)
+    tb_name   = "{}_tb".format(check_name)
+    build_dir = "build/{}".format(tb_name)
 
     platform.build(
-        top       = top,
-        name      = top_name,
+        top       = tb,
+        name      = tb_name,
         build_dir = build_dir,
         mode      = "bmc",
         ghdl_opts = "--std=08",
@@ -92,28 +93,26 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     checks = [
-        # name           pre   post
-        ("unique",       12,   15),
-        ("ia_fwd",       None, 15),
-        ("gpr",          None, 15),
-        ("cr",           None, 15),
-        ("spr",          None, 15),
+        ("cons_unique",  {"post": 15, "pre": 12}),
+        ("cons_ia_fwd",  {"post": 15}),
+        ("cons_gpr",     {"post": 15}),
+        ("cons_cr",      {"post": 15}),
+        ("cons_spr",     {"post": 15}),
 
-        ("insn_b",       None, 15),
-        ("insn_ba",      None, 15),
-        ("insn_bl",      None, 15),
-        ("insn_bla",     None, 15),
-        ("insn_bc",      None, 15),
-        ("insn_bca",     None, 15),
-        ("insn_bcl",     None, 15),
-        ("insn_bcla",    None, 15),
-
-        ("insn_bclr",    None, 15),
-        ("insn_bclrl",   None, 15),
-        ("insn_bcctr",   None, 15),
-        ("insn_bcctrl",  None, 15),
-        ("insn_bctar",   None, 15),
-        ("insn_bctarl",  None, 15),
+        ("insn_b",       {"post": 15}),
+        ("insn_ba",      {"post": 15}),
+        ("insn_bl",      {"post": 15}),
+        ("insn_bla",     {"post": 15}),
+        ("insn_bc",      {"post": 15}),
+        ("insn_bca",     {"post": 15}),
+        ("insn_bcl",     {"post": 15}),
+        ("insn_bcla",    {"post": 15}),
+        ("insn_bclr",    {"post": 15}),
+        ("insn_bclrl",   {"post": 15}),
+        ("insn_bcctr",   {"post": 15}),
+        ("insn_bcctrl",  {"post": 15}),
+        ("insn_bctar",   {"post": 15}),
+        ("insn_bctarl",  {"post": 15}),
     ]
 
     with multiprocessing.Pool(processes=args.jobs) as pool:

power_fv/build/plat.py
@@ -116,7 +116,7 @@ class SymbiYosysPlatform(TemplatedPlatform):
         if not isinstance(top, tb.Testbench):
             raise TypeError("Top-level must be an instance of power_fv.tb.Testbench")
 
-        skip  = str(top.t_post)
-        depth = str(top.t_post + 1)
+        depth = str(top.bmc_depth)
+        skip  = str(top.bmc_depth - 1)
 
         return super().build(top, mode=mode, depth=depth, skip=skip, **kwargs)

power_fv/checks/__init__.py
@@ -1,5 +1,12 @@
-from .cr     import *
-from .gpr    import *
-from .ia_fwd import *
-from .spr    import *
-from .unique import *
+class PowerFVCheck:
+    registry = {}
+    name     = None
+
+    def __init_subclass__(cls, name):
+        if name in cls.registry:
+            raise ValueError("Check name {!r} is already registered".format(name))
+        cls.registry[name] = cls
+        cls.name = name
+
+    def get_testbench(self, dut, *args, **kwargs):
+        raise NotImplementedError

power_fv/checks/all.py
@@ -1,0 +1,2 @@
+from .cons.all import *
+from .insn.all import *

power_fv/checks/cons/all.py
@@ -1,0 +1,5 @@
+from .unique import *
+from .ia_fwd import *
+from .gpr    import *
+from .cr     import *
+from .spr    import *

power_fv/checks/cons/cr.py
@@ -1,23 +1,24 @@
 from amaranth import *
 from amaranth.asserts import *
 
-from .. import pfv
+from .. import PowerFVCheck
+from ... import pfv, tb
 
 
-__all__ = ["Check"]
+__all__ = ["CRSpec", "CRCheck"]
 
 
-class Check(Elaboratable):
-    """Condition Register check.
+class CRSpec(Elaboratable):
+    """Condition Register consistency specification.
 
-    Checks that reads from CR fields are consistent with the last value that was written to them.
+    Checks that reads from CR fields return the last value that was written to them.
     """
-    def __init__(self):
+    def __init__(self, post):
         self.pfv  = pfv.Interface()
-        self.trig = Record([
-            ("pre",  1),
-            ("post", 1),
-        ])
+        self.post = tb.Trigger(cycle=post)
+
+    def triggers(self):
+        yield self.post
 
     def elaborate(self, platform):
         m = Module()
@@ -56,7 +57,7 @@ class Check(Elaboratable):
                         cr_field.shadow .eq(cr_field.pfv.w_data),
                     ]
 
-        with m.If(self.trig.post):
+        with m.If(self.post.stb):
             m.d.sync += [
                 Assume(Past(self.pfv.stb)),
                 Assume(Past(self.pfv.order) == spec_order),
@@ -68,3 +69,10 @@ class Check(Elaboratable):
                         m.d.sync += Assert(Past(cr_field.pfv.r_data) == Past(cr_field.shadow))
 
         return m
+
+
+class CRCheck(PowerFVCheck, name="cons_cr"):
+    def get_testbench(self, dut, post):
+        tb_spec = CRSpec(post)
+        tb_top  = tb.Testbench(tb_spec, dut)
+        return tb_top

power_fv/checks/cons/gpr.py
@@ -1,26 +1,27 @@
 from amaranth import *
 from amaranth.asserts import *
 
-from .. import pfv
+from .. import PowerFVCheck
+from ... import pfv, tb
 
 
-__all__ = ["Check"]
+__all__ = ["GPRSpec", "GPRCheck"]
 
 
-class Check(Elaboratable):
-    """General Purpose Registers check.
+class GPRSpec(Elaboratable):
+    """GPR consistency specification.
 
     Checks that:
-      * reads from a GPR are consistent with the last value that was written to it;
+      * reads from a GPR return the last value that was written to it;
       * writes to multiple GPRs by a single instruction (e.g. load with update) do not target the
         same register.
     """
-    def __init__(self):
+    def __init__(self, post):
         self.pfv  = pfv.Interface()
-        self.trig = Record([
-            ("pre",  1),
-            ("post", 1),
-        ])
+        self.post = tb.Trigger(cycle=post)
+
+    def triggers(self):
+        yield self.post
 
     def elaborate(self, platform):
         m = Module()
@@ -58,7 +59,7 @@ class Check(Elaboratable):
                 with m.Else():
                     m.d.sync += gpr_shadow.eq(self.pfv.rt.w_data)
 
-        with m.If(self.trig.post):
+        with m.If(self.post.stb):
             m.d.sync += [
                 Assume(Past(self.pfv.stb)),
                 Assume(Past(self.pfv.order) == spec_order),
@@ -75,3 +76,10 @@ class Check(Elaboratable):
                     m.d.sync += Assert(Past(gpr_shadow) == Past(self.pfv.rt.r_data))
 
         return m
+
+
+class GPRCheck(PowerFVCheck, name="cons_gpr"):
+    def get_testbench(self, dut, post):
+        tb_spec = GPRSpec(post)
+        tb_top  = tb.Testbench(tb_spec, dut)
+        return tb_top

commit 010c383ed7bc75c84ed723a55a53d6a2a2d62ce7

tb: remove testbench start trigger.
We don't use it currently; we just assume the sync domain is under
reset at the beginning of the BMC.

Also, fix a regression in the cycle counter introduced by 9ea58a47.

power_fv/tb.py
@@ -10,8 +10,8 @@ __all__ = ["Trigger", "Testbench"]
 
 class Trigger:
     def __init__(self, cycle, name=None, src_loc_at=0):
-        if not isinstance(cycle, int) or cycle < 0:
-            raise ValueError("Clock cycle must be a non-negative integer, not {!r}"
+        if not isinstance(cycle, int) or cycle <= 0:
+            raise ValueError("Clock cycle must be a positive integer, not {!r}"
                              .format(cycle))
 
         self.stb   = Signal(name=name, src_loc_at=1 + src_loc_at)
@@ -19,16 +19,14 @@ class Trigger:
 
 
 class Testbench(Elaboratable):
-    def __init__(self, spec, dut, start=0):
-        self.spec  = spec
-        self.dut   = dut
-        self.start = Trigger(cycle=start)
+    def __init__(self, spec, dut):
+        self.spec = spec
+        self.dut  = dut
 
         self._triggers  = OrderedDict()
         self._bmc_depth = None
         self._frozen    = False
 
-        self.add_trigger(self.start)
         for trigger in spec.triggers():
             self.add_trigger(trigger)
 
@@ -56,20 +54,15 @@ class Testbench(Elaboratable):
     def elaborate(self, platform):
         m = Module()
 
-        cycle = Signal(range(self.bmc_depth), reset=0)
+        cycle = Signal(range(self.bmc_depth), reset=1)
 
-        with m.If(cycle < self.bmc_depth):
+        with m.If(cycle < self.bmc_depth - 1):
             m.d.sync += cycle.eq(cycle + 1)
 
         for trigger in self.triggers():
             m.d.comb += trigger.stb.eq(cycle == trigger.cycle)
 
-        spec_rst = Signal(reset=1)
-
-        with m.If(self.start.stb):
-            m.d.sync += spec_rst.eq(0)
-
-        m.submodules.spec = ResetInserter(spec_rst)(self.spec)
+        m.submodules.spec = self.spec
         m.submodules.dut  = self.dut
 
         m.d.comb += self.spec.pfv.eq(self.dut.pfv)